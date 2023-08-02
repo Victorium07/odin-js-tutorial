7f8aebbc8cc7abfdf90f7fabcb7fe2b03bfe4f7b
Victor <victor.h.alves7@gmail.com>
2023-08-02 18:28:45 -0300

    game eval function added

--- rps.js
+++ rps.js
@@ -10,6 +10,22 @@ function adjustWord(word) {
     return(adjustedWord)
 }
 
+function evaluateGame(comp, player) {
+    if (comp === player) {return("Draw")}
+    else if (comp === "rock") {
+        if (player === "scissors") {return("Computer wins!")}
+        else {return("Player wins!")}
+    }
+    else if (comp === "scissors") {
+        if (player === "paper") {return("Computer wins!")}
+        else {return("Player wins!")}
+    }
+    else if (comp === "paper") {
+        if (player === "rock") {return("Computer wins!")}
+        else {return("Player wins!")}
+    }
+}
+
 let repetitions = parseInt(window.prompt("How many times do you wanna play?"));
 let playerSelection = adjustWord(window.prompt("Pick your option! (Rock, paper or scissors)"));
 
